9d94dcd1fc47455fa68583476344ec3081481fbf
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-04-19 23:03:28 +0200

    start with full setup

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
 import re
 import sys
-
-pkg_file = open("slack_cleaner2/__init__.py").read()
-metadata = dict(re.findall("__([a-z]+)__\s*=\s*'([^']+)'", pkg_file))
-description = open('README.md').read()
-
+import io
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+pkg_file = io.open("slack_cleaner2/__init__.py", encoding='utf-8').read()
+metadata = dict(re.findall("__([a-z]+)__\s*=\s*'([^']+)'", pkg_file))
+description = io.open('README.md', encoding='utf-8').read()
+
+
+class Tox(TestCommand):
+  def finalize_options(self):
+    TestCommand.finalize_options(self)
+    self.test_args = []
+    self.test_suite = True
+
+  def run_tests(self):
+    #import here, cause outside the eggs aren't loaded
+    import tox
+    errcode = tox.cmdline(self.test_args)
+    sys.exit(errcode)
+
 
 setup(
     name='slack_cleaner2',
@@ -19,13 +34,19 @@ setup(
     keywords="slack, clean, delete, message, file",
     long_description=description,
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
     ],
-    install_requires=['slacker', 'colorama', 'setuptools'],
+    tests_require=['tox'],
+    test_suite='slack_cleaner2.test.test_slack_cleaner2',
+    extras_require={
+        'testing': ['tox'],
+    },
+    cmdclass={'test': Tox},
+    install_requires=['slacker', 'colorama'],
     entry_points={
         'console_scripts': [
             'slack-cleaner = slack_cleaner2.cli:run'
